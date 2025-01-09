14bacb2bd19289136293b6e7cc41b47f5e7277ab
Lean <leian1306@gmail.com>
2025-01-09 11:35:00 -0300

    auth: use recommended setup

--- src/lib/auth/signin.ts
+++ src/lib/auth/signin.ts
@@ -1,13 +1,26 @@
 import { supabaseClient } from '../supabase/client';
 
+const getURL = () => {
+    const url =
+      process.env.NEXT_PUBLIC_SITE_URL ?? // Site URL in production env
+      process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel
+      'http://localhost:3000/';
+
+    const startsWithHttp = url.startsWith('http');
+    const endsWithSlash = url.endsWith('/');
+
+    return `${startsWithHttp ? url : `https://${url}`}${endsWithSlash ? '' : '/'}`;
+};
+
 export async function signInWithLinkedIn() {
+    const url = getURL();
     await supabaseClient.auth.signInWithOAuth({
         provider: 'linkedin_oidc',
         options: {
             scopes: 'openid profile email r_organization_social r_basicprofile w_member_social w_organization_social',
-            redirectTo: process.env.OAUTH_CALLBACK_URL,
+            redirectTo: url,
             queryParams: {
-                next: `https://${process.env.VERCEL_URL ?? 'localhost:3000'}/p`
+                next: `${url}p`
             }
         }
     });
