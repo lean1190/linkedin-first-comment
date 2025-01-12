ac2db09b77167db61aaf74e4528c44b67102267a
Lean <leian1306@gmail.com>
2025-01-12 12:29:55 -0300

    Configure Supabase types

--- src/lib/supabase/types.ts
+++ src/lib/supabase/types.ts
@@ -1,28 +1,190 @@
 export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
 
-export interface Database {
+export type Database = {
   public: {
     Tables: {
-      movies: {
+      Comments: {
         Row: {
-          // the data expected from .select()
-          id: number;
-          name: string;
-          data: Json | null;
+          author: string;
+          content: string;
+          created_at: string;
+          id: string;
+          post: string | null;
         };
         Insert: {
-          // the data to be passed to .insert()
-          id?: never; // generated columns must not be supplied
-          name: string; // `not null` columns with no default must be supplied
-          data?: Json | null; // nullable columns can be omitted
+          author?: string;
+          content: string;
+          created_at?: string;
+          id?: string;
+          post?: string | null;
         };
         Update: {
-          // the data to be passed to .update()
-          id?: never;
-          name?: string; // `not null` columns are optional on .update()
-          data?: Json | null;
+          author?: string;
+          content?: string;
+          created_at?: string;
+          id?: string;
+          post?: string | null;
         };
+        Relationships: [
+          {
+            foreignKeyName: 'Comments_author_fkey';
+            columns: ['author'];
+            isOneToOne: false;
+            referencedRelation: 'Users';
+            referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'Comments_post_fkey';
+            columns: ['post'];
+            isOneToOne: false;
+            referencedRelation: 'Posts';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
+      Posts: {
+        Row: {
+          content: string;
+          created_at: string;
+          id: string;
+          post_at_utc: string;
+          repost_at_utc: string | null;
+        };
+        Insert: {
+          content: string;
+          created_at?: string;
+          id?: string;
+          post_at_utc: string;
+          repost_at_utc?: string | null;
+        };
+        Update: {
+          content?: string;
+          created_at?: string;
+          id?: string;
+          post_at_utc?: string;
+          repost_at_utc?: string | null;
+        };
+        Relationships: [];
+      };
+      Users: {
+        Row: {
+          created_at: string;
+          id: string;
+          timezone: string;
+        };
+        Insert: {
+          created_at?: string;
+          id?: string;
+          timezone: string;
+        };
+        Update: {
+          created_at?: string;
+          id?: string;
+          timezone?: string;
+        };
+        Relationships: [];
       };
     };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
   };
-}
+};
+
+type PublicSchema = Database[Extract<keyof Database, 'public'>];
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema['Tables'] & PublicSchema['Views'])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+        Database[PublicTableNameOrOptions['schema']]['Views'])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+      Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+      Row: infer R;
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema['Tables'] & PublicSchema['Views'])
+    ? (PublicSchema['Tables'] & PublicSchema['Views'])[PublicTableNameOrOptions] extends {
+        Row: infer R;
+      }
+      ? R
+      : never
+    : never;
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends keyof PublicSchema['Tables'] | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Insert: infer I;
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+    ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+        Insert: infer I;
+      }
+      ? I
+      : never
+    : never;
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends keyof PublicSchema['Tables'] | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Update: infer U;
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+    ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+        Update: infer U;
+      }
+      ? U
+      : never
+    : never;
+
+export type Enums<
+  PublicEnumNameOrOptions extends keyof PublicSchema['Enums'] | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema['Enums']
+    ? PublicSchema['Enums'][PublicEnumNameOrOptions]
+    : never;
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema['CompositeTypes']
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database;
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes']
+    : never = never
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes'][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema['CompositeTypes']
+    ? PublicSchema['CompositeTypes'][PublicCompositeTypeNameOrOptions]
+    : never;
