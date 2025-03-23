32c693f252a4fa8581d8e01517739814b0e4d265
Lean <leian1306@gmail.com>
2025-03-23 18:39:56 +0100

    posts: fix mobile skeleton

--- src/app/p/posts/loading.tsx
+++ src/app/p/posts/loading.tsx
@@ -1,5 +1,18 @@
 import { pageContainerBgColor, pageContainerWidthDesktop } from '@/lib/constants/containers';
 
+const skeleton = (
+  <div className="flex py-4 justify-between items-start gap-4 mb-2">
+    <div className="flex flex-col items-start gap-y-2 min-w-0">
+      <div className="h-4 w-32 bg-neutral-700 rounded" />
+      <div className="h-4 w-44 sm:w-56 bg-neutral-700 rounded" />
+    </div>
+
+    <div className="flex items-center gap-1">
+      <div className="h-5 w-14 rounded-full bg-neutral-700" />
+    </div>
+  </div>
+);
+
 export default function PostsLoading() {
   return (
     <article
@@ -7,38 +20,9 @@ export default function PostsLoading() {
     >
       <div className="mb-8 h-10 bg-neutral-700 rounded w-1/2" />
 
-      <div className="flex py-4 justify-between items-start gap-4 mb-2">
-        <div className="flex flex-col items-start gap-y-2 min-w-0">
-          <div className="h-4 w-32 bg-neutral-700 rounded" />
-          <div className="h-4 w-80 bg-neutral-700 rounded" />
-        </div>
-
-        <div className="flex items-center gap-1">
-          <div className="h-5 w-14 rounded-full bg-neutral-700" />
-        </div>
-      </div>
-
-      <div className="flex py-4 justify-between items-start gap-4 mb-2">
-        <div className="flex flex-col items-start gap-y-2 min-w-0">
-          <div className="h-4 w-32 bg-neutral-700 rounded" />
-          <div className="h-4 w-80 bg-neutral-700 rounded" />
-        </div>
-
-        <div className="flex items-center gap-1">
-          <div className="h-5 w-14 rounded-full bg-neutral-700" />
-        </div>
-      </div>
-
-      <div className="flex py-4 justify-between items-start gap-4 mb-2">
-        <div className="flex flex-col items-start gap-y-2 min-w-0">
-          <div className="h-4 w-32 bg-neutral-700 rounded" />
-          <div className="h-4 w-80 bg-neutral-700 rounded" />
-        </div>
-
-        <div className="flex items-center gap-1">
-          <div className="h-5 w-14 rounded-full bg-neutral-700" />
-        </div>
-      </div>
+      {skeleton}
+      {skeleton}
+      {skeleton}
     </article>
   );
 }
