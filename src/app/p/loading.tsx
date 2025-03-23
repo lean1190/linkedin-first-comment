ac653a2cecbf05261dfd0971fcf1d0d6316335b0
Lean <leian1306@gmail.com>
2025-03-23 18:50:05 +0100

    p: improve loading skeleton

--- src/app/p/loading.tsx
+++ src/app/p/loading.tsx
@@ -15,8 +15,8 @@ export default function PlatformLoading() {
       <section className="mb-2 flex items-center gap-2">
         <div className="h-12 w-12 rounded-full bg-neutral-700" />
         <div>
-          <div className="h-3 mb-1 w-62 max-w-full bg-neutral-700 rounded-lg" />
-          <div className="h-2 mb-1 w-62 max-w-full bg-neutral-700 rounded-lg" />
+          <div className="h-3 mb-1 w-32 max-w-full bg-neutral-700 rounded-lg" />
+          <div className="h-2 mb-1 w-32 max-w-full bg-neutral-700 rounded-lg" />
           <div className="h-2 mb-1 w-12 max-w-full bg-neutral-700 rounded-lg" />
         </div>
       </section>
@@ -27,7 +27,7 @@ export default function PlatformLoading() {
         <div className="h-4 w-72 max-w-full bg-neutral-700 rounded-lg" />
       </section>
 
-      <section className="flex items-center justify-between py-2">
+      <section className="flex items-center justify-between py-4">
         <div className="flex items-center gap-1">
           <div className="flex">
             <div className="h-4 w-4 bg-neutral-700 rounded-xl" />
@@ -39,17 +39,17 @@ export default function PlatformLoading() {
         <div className="h-4 bg-neutral-700 rounded-lg w-1/4" />
       </section>
 
-      <div className="h-0.5 bg-neutral-700 rounded-xl w-full my-2" />
+      <div className="h-0.5 bg-neutral-700 rounded-xl w-full my-4" />
 
       <section className="mb-2 flex items-center gap-2">
         <div className="h-8 w-8 rounded-full bg-neutral-700" />
         <div>
-          <div className="h-2 mb-1 w-62 max-w-full bg-neutral-700 rounded-lg" />
-          <div className="h-2 mb-1 w-62 max-w-full bg-neutral-700 rounded-lg" />
+          <div className="h-2 mb-1 w-32 max-w-full bg-neutral-700 rounded-lg" />
+          <div className="h-2 mb-1 w-32 max-w-full bg-neutral-700 rounded-lg" />
         </div>
       </section>
 
-      <section>
+      <section className="pl-10">
         <div className="h-2 mb-1 w-62 max-w-full bg-neutral-700 rounded-lg" />
         <div className="h-2 mb-1 w-72 max-w-full bg-neutral-700 rounded-lg" />
         <div className="h-2 w-72 max-w-full bg-neutral-700 rounded-lg" />
