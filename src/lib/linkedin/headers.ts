54056dc3940d3b08a645902d856991887e83669a
Lean <leian1306@gmail.com>
2025-10-18 10:59:12 +0200

    Extract LinkedIn api version to a constant

--- src/lib/linkedin/headers.ts
+++ src/lib/linkedin/headers.ts
@@ -1,10 +1,12 @@
+import { apiVersion } from './constants';
+
 export function linkedInHeaders(token: string) {
   return {
     Authorization: `Bearer ${token}`,
     Accept: '*/*',
     'User-Agent': 'First-Comment/1.0',
     'Content-Type': 'application/json',
-    'LinkedIn-Version': '202504',
+    'LinkedIn-Version': apiVersion,
     'X-Restli-Protocol-Version': '2.0.0',
     'Access-Control-Allow-Origin': '*'
   };
